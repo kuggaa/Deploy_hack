0f804494fd517e646b4e02248983598beeb45da0
Кузьмицкий Артем <user23234@mail50.stend>
2014-01-21 17:04:01 +0400

    changed impl of firefix_vnc palybook now its work

--- deployster/roles/firefox_vnc/files/firefox.js
+++ deployster/roles/firefox_vnc/files/firefox.js
@@ -1,4 +1,4 @@
 pref("network.protocol-handler.expose.vnc", false);
 
 user_pref("browser.startup.page", 1);
-user_pref("browser.startup.homepage", "file:///etc/firefox/pref/index.html");
+user_pref("browser.startup.homepage", "file:///usr/lib/firefox/defaults/pref/index.html");
