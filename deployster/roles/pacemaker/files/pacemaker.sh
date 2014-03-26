d8fb81217212fcc415cfbb88ec042da0df532b19
Кузьмицкий Артем <user23234@mail50.stend>
2014-03-26 16:27:53 +0400

    fight with fencing and update for compatibility with glusterfs 3.4

--- deployster/roles/pacemaker/files/pacemaker.sh
+++ deployster/roles/pacemaker/files/pacemaker.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cibadmin -U -X '<cib validate-with="pacemaker-1.1"/>'
 cibadmin --replace --scope resources --xml-file /tmp/resources.xml
 cibadmin --replace --scope constraints --xml-file /tmp/constraints.xml
 cibadmin -M -c -o configuration --xml-file /tmp/fencing_topology.xml
